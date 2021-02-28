c13d94013943e0687ea6d880f6acd7d338dcd805
Hyyy_1 <447542807@qq.com>
2021-02-28 16:56:06 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Rain />
+</template>
+
+<script>
+import Rain from './components/Rain.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Rain,
+  },
+}
+</script>
---
+++ src/components/Rain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>我的第一个杀杀杀Vue3组件</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '',
+    }
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+}
+</style>
