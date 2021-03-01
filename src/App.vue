4bd8ecdf078ed34b874a26c6bed0e29e326d6082
Hyyy_1 <447542807@qq.com>
2021-03-01 15:21:47 +0800

    更新了一些css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div>
-    hi | <router-link to="/">到我的rain1</router-link>|<router-link to="/x"
-      >到我的rain2</router-link
-    >
-  </div>
-  <hr />
   <router-view></router-view>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Rain from './components/Rain.vue'
-import Rain2 from './components/Rain2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
-console.log(createRouter)
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
   routes: [
-    { path: '/', component: Rain },
-    { path: '/x', component: Rain2 },
+    { path: '/', component: Home },
+    { path: '/doc', component: Doc },
   ],
 })
 
