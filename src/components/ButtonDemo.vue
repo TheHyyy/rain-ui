1f555e14e1bc56b4594bc111f18726983c0187aa
Hyyy_1 <447542807@qq.com>
2021-03-06 18:03:46 +0800

    Button基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,10 @@
   <div>Button示例</div>
   <h1>示例</h1>
   <div>
-    <Button @click="onClick" @focus="onClick" @mouseover="onClick">你好</Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
 
