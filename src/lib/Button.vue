487aee97f84bdbd6a7382b59eeb039aab0554f9c
Hyyy_1 <447542807@qq.com>
2021-03-06 19:44:51 +0800

    支持disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,40 @@
 <template>
-  <button class="rain-button" :class="{ [`rain-theme-${theme}`]: theme }">
+  <button class="rain-button" :class="classes" :disabled="disabled">
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
 export default {
   props: {
     theme: {
       type: String,
       default: 'button',
     },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    level: {
+      type: String,
+      default: 'normal',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const { theme, size, level } = props
+    const classes = computed(() => {
+      return {
+        [`rain-theme-${theme}`]: theme,
+        [`rain-size-${size}`]: size,
+        [`rain-level-${level}`]: level,
+      }
+    })
+    return { classes }
   },
 }
 </script>
@@ -21,6 +45,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .rain-button {
   box-sizing: border-box;
   height: $h;
@@ -35,6 +61,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -48,6 +75,97 @@ $radius: 4px;
   }
   &::-moz-focus-inner {
     border: 0;
+  }
+  &.rain-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.rain-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.rain-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.rain-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.rain-theme-button {
+    &.rain-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.rain-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.rain-theme-link {
+    &.rain-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.rain-theme-text {
+    &.rain-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.rain-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.rain-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.rain-theme-link,
+  &.rain-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
   }
 }
 </style>
