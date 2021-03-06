6e55b2c6a1fb1a8f5bc8cc0bc21c2c6682d4d87f
Hyyy_1 <447542807@qq.com>
2021-03-06 19:51:30 +0800

    基本完善Button组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="rain-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="rain-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -21,6 +22,10 @@ export default {
       default: 'normal',
     },
     disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
       type: Boolean,
       default: false,
     },
@@ -166,6 +171,25 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+  > .rain-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: rain-spin 1s infinite linear;
+  }
+}
+@keyframes rain-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
